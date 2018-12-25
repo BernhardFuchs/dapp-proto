e7202b3aa7f7098247a0a83d95e62cf421c52059
Bernhard Fuchs <bernhard.fuchs@paysafe.com>
2018-12-25 21:13:19 +0100

    git Page deploy

--- src/app/dapp-dashboard/dapp-dashboard.module.ts
+++ src/app/dapp-dashboard/dapp-dashboard.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+
+import { GuiModule } from '../gui/gui.module';
 import { SharedModule } from '../shared/shared.module';
 import { DappDashboardComponent } from './dapp-dashboard.component';
 import { TokenTableComponent } from './token-table/token-table.component';
-import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
-import { GuiModule } from '../gui/gui.module';
-import { DappDashboardRoutingModule } from './dapp-dashboard.routing.module';
+
+const routes = [
+  {
+    path: '',
+    component: DappDashboardComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +24,7 @@ import { DappDashboardRoutingModule } from './dapp-dashboard.routing.module';
     DappDashboardComponent
   ],
   imports: [
-    DappDashboardRoutingModule,
+    RouterModule.forChild(routes),
     CommonModule,
     SharedModule,
     GuiModule,
