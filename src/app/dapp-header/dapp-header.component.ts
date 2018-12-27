4adc18020fee45b2121a4fdd1dbd57ce9d37b996
Bernhard Fuchs <bernhard.fuchs@paysafe.com>
2018-12-27 16:54:11 +0100

    fix subscription error

--- src/app/dapp-header/dapp-header.component.ts
+++ src/app/dapp-header/dapp-header.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store, Select } from '@ngxs/store';
 import { ChainInfoFetchAction, ChainInfoState } from '../dapp-store';
-import { fromEvent, Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
-const SCROLL_CONTAINER = '.mat-drawer-content';
-const PRIMARY_TEXT_THRESHOLD = 22;
 @Component({
   selector: 'app-dapp-header',
   templateUrl: './dapp-header.component.html',
@@ -24,23 +21,10 @@ export class DappHeaderComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.store$.dispatch(new ChainInfoFetchAction(this.payload));
-
-    const container = document.querySelector(SCROLL_CONTAINER);
-    fromEvent(container, 'scroll').pipe(
-      takeUntil(this._onDestroy)
-    ).subscribe(() => this.determineHeader(container.scrollTop));
   }
 
   ngOnDestroy() {
     this._onDestroy.next();
-  }
-
-  private determineHeader(top: number) {
-    if (top >= PRIMARY_TEXT_THRESHOLD) {
-      this.popText = true;
-    } else {
-      this.popText = false;
-    }
   }
 
 }
