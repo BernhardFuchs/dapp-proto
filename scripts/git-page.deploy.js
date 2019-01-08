9a693f7963d1ed9afa6209523b4743ec7a4acf5e
Bernhard Fuchs <bernhard.fuchs@paysafe.com>
2019-01-08 20:08:47 +0100

    GitPage deploy

--- scripts/git-page.deploy.js
+++ scripts/git-page.deploy.js
@@ -3,28 +3,27 @@ const exec = require('child_process').exec;
 
 const commitMessage = 'GitPage deploy';
 console.log(`Committing with message ${commitMessage}`.bgGreen);
-exec(`git add .`,
-  (error, stdout, stderr) => {
+exec(`git add .`, (error, stdout, stderr) => {
+  handleError(error);
+  logstd(stdout, stderr);
+  exec(`git commit -m '${commitMessage}'`, (error, stdout, stderr) => {
     handleError(error);
     logstd(stdout, stderr);
-    exec(`git commit -m ${commitMessage}`, (error, stdout, stderr) => {
+    exec(`git push`, (error, stdout, stderr) => {
       handleError(error);
       logstd(stdout, stderr);
-      exec(`git push`, (error, stdout, stderr) => {
-        handleError(error);
-        logstd(stdout, stderr);
-      })
-    })
+    });
   });
+});
 
-  const logstd = (stdout, stderr) => {
-    console.log(`${stdout}`.green);
-    console.log(`${stderr}`.bgRed);
+const logstd = (stdout, stderr) => {
+  console.log(`${stdout}`.green);
+  console.log(`${stderr}`.bgRed);
+};
+
+const handleError = error => {
+  if (error !== null) {
+    console.log(`${error}`.red);
+    process.exit(1);
   }
-  
-  const handleError = (error) => {
-    if (error !== null) {
-      console.log(`${error}`.red);
-      process.exit(1);
-    }
-  }
+};
